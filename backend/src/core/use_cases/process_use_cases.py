a3ba30004fb65380053694ed2ebd75109c2f9570
PedroTort <pedper@alunos.utfpr.edu.br>
2026-07-26 14:09:36 -0300

    fix: correct student_course_id assignment in process creation and update methods

--- backend/src/core/use_cases/process_use_cases.py
+++ backend/src/core/use_cases/process_use_cases.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 class ProcessUseCases:
     @staticmethod
     def create_new_process(request: CreateProcessRequest) -> dict:
-        student_course_id = CourseIds[request.student_course.value].value
+        student_course = Course(request.student_course).value
         student_id = AuthenticationTasks.create_or_update_user_from_process(
             name=request.student_name,
             email=request.student_email,
@@ -88,7 +88,7 @@ class ProcessUseCases:
     def update_process(process_id: int, request: UpdateProcessRequest) -> dict:
         print(request)
         process = ProcessTasks.get_process_by_id(process_id)
-        student_course_id = CourseIds[request.student_course.value].value
+        student_course = Course(request.student_course).value
         if not process:
             raise ValueError("Process not found")
         
@@ -100,7 +100,7 @@ class ProcessUseCases:
             ra=request.student_ra,
             role_id=UserRoleId.STUDENT.value,
             student_period=request.student_period,
-            student_course_id=student_course_id
+            student_course_id=student_course
         )["id"]
 
         new_advisor_id = AuthenticationTasks.create_or_update_user_from_process(
